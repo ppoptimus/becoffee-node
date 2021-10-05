6ffed98ad2463b1759b05b3c39dff7cee1acaa8c
ppoptimus <r.pumpo@gmail.com>
2021-10-05 10:45:15 +0700

    remove config

--- pushMessageMulticast.js
+++ pushMessageMulticast.js
@@ -1,46 +1,52 @@
-const axios = require("axios")
-const appConfig = require("./config")
+const axios = require('axios');
 
 const PushMessageMulticast = (req, res) => {
-    const user_id = req.body.user_id
-    const message = req.body.message
-    const originalContentUrl = req.body.originalContentUrl
-    const previewImageUrl = req.body.previewImageUrl
-    
-	const data = JSON.stringify({
-		to: user_id,
-		messages: [
-			{
-				type: "text",
-				text: message,
-			},
-			{
-				type: "image",
-				originalContentUrl: originalContentUrl,
-				previewImageUrl: previewImageUrl,
-			},
-		],
-	})
+  const header = req.headers.authorization;
+  const url_enpoint = req.body.url_enpoint;
+  const user_id = req.body.user_id;
+  const message = req.body.message;
+  const originalContentUrl = req.body.originalContentUrl;
+  const previewImageUrl = req.body.previewImageUrl;
 
-	const config = {
-		method: "post",
-		url: appConfig.LINE_MESSAGE_MULTICAST_URL,
-		headers: appConfig.LINE_MESSAGE_HEADER,
-		data: data,
-	}
+  const data = JSON.stringify({
+    to: user_id,
+    messages: [
+      {
+        type: 'text',
+        text: message,
+      },
+      {
+        type: 'image',
+        originalContentUrl: originalContentUrl,
+        previewImageUrl: previewImageUrl,
+      },
+    ],
+  });
 
-	axios(config)
-		.then(function (response) {
-			return res.status(200).json(response.data);
-		})
-		.catch(function (error) {
-            if (error.response) {
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            }
-            return res.status(error.response.status).json(error.response.data);
-          });
-} 
+  const config = {
+    method: 'post',
+    url: url_enpoint,
+    headers: {
+      Authorization: header,
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      return res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        return res.status(500).json(error);
+      }
+      return res.status(error.response.status).json(error.response.data);
+    });
+};
 
 module.exports = PushMessageMulticast;
